ef5cafb592c1872b6facc17632cd2ab0c58a0754
Alejandro <20230154@ricaldone.edu.sv>
2025-05-28 10:53:32 -0600

    Solo falta probarlo

---
+++ backend/src/database.js
@@ -0,0 +1,27 @@
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import {config} from "./src/config.js";
+mongoose.connect(config.db.URI)
+
+const connection = mongoose.connection;
+
+connection.once("open", () =>
+{
+    console.log("Connected");
+}
+);
+
+connection.once("disconnected", () =>
+  {
+    console.log("disconnected");
+  }
+);
+
+connection.once("error", (error) =>
+{
+    console.log("error found" + error);
+}
+);
